66f1f86d145f93fc15fdf4f1d97324279f200a6a
chunjx <chung.joseph@gmail.com>
2016-03-06 19:42:25 -0600

    Added forecast
    Added forecast

--- source/weather.py
+++ source/weather.py
@@ -42,29 +42,29 @@ background = pygame.Surface(screen.get_size())
 background = background.convert()
 background.fill((250, 250, 250))
 
-#Set font
-base_font = pygame.font.Font(None, 36)
+#Set fonts
+base_font = pygame.font.Font(None, 30)
 ctemp_font = pygame.font.Font(None, 48)
+small_font = pygame.font.Font(None, 24)
 
 #Display city
 def getcity():
     city = base_font.render("Deerfield", 1, (10, 10, 10))
     citypos = city.get_rect()
     citypos.centerx = background.get_rect().centerx*1.7
-    citypos.centery = background.get_rect().centery/4
+    citypos.centery = background.get_rect().centery/5
     background.blit(city, citypos)
 
 #Display time
-def gettime():
-    currenttime = time.strftime("%I:%M %p")
+def gettime():    
+    currenttime = time.strftime("%A, %B %d %I:%M %p")
     clock = base_font.render(currenttime, 1, (10, 10, 10))
     clockpos = clock.get_rect()
-    clockpos.centerx = background.get_rect().centerx/3.9
-    clockpos.centery = background.get_rect().centery/4
+    clockpos.centerx = background.get_rect().centerx/1.7
+    clockpos.centery = background.get_rect().centery/5
     background.blit(clock, clockpos)
 
-
-#Get current conditions
+#Get current conditions from Weather Underground
 def getcurrentconditionsdata():
     #Call Weather Underground conditions API
     conditionsurl = 'http://api.wunderground.com/api/'+api_key+'/conditions/q/zmw:60015.1.99999.json'
@@ -81,15 +81,8 @@ def getcurrentconditionsdata():
     return js
 
 
+#Display current weather conditions
 def displayweather(jsondata):
-    #Read weather condition
-    weather_data = jsondata["current_observation"]["weather"]  
-    weather = base_font.render(weather_data, 1, (10, 10, 10))
-    weatherpos = weather.get_rect()
-    weatherpos.centerx = background.get_rect().centerx
-    weatherpos.centery = background.get_rect().centery-20
-    background.blit(weather, weatherpos)
-
     #Display current weather icon
     icon_url = jsondata["current_observation"]["icon_url"]
     icon_str = urllib.urlopen(icon_url).read()
@@ -100,16 +93,32 @@ def displayweather(jsondata):
     iconpos.centerx = background.get_rect().centerx
     iconpos.centery = background.get_rect().centery-60
     background.blit(icon_image, iconpos)
+    
+    #Read weather condition
+    weather_data = jsondata["current_observation"]["weather"]  
+    weather = base_font.render(weather_data, 1, (10, 10, 10))
+    weatherpos = weather.get_rect()
+    weatherpos.centerx = background.get_rect().centerx
+    weatherpos.centery = background.get_rect().centery
+    background.blit(weather, weatherpos)
 
     #Display current temp
     temp_data = str(jsondata["current_observation"]["temp_f"])+unichr(176)
     temp = ctemp_font.render(temp_data, 1, (10,10,10))
     temp_pos = temp.get_rect()
     temp_pos.centerx = background.get_rect().centerx/4
-    temp_pos.centery = background.get_rect().centery
+    temp_pos.centery = background.get_rect().centery-55
     background.blit(temp, temp_pos)
+    
+    feelslike_data = "Feels like "+str(jsondata["current_observation"]["feelslike_f"])+unichr(176)
+    feelslike = small_font.render(feelslike_data, 1, (10,10,10))
+    feelslikepos = feelslike.get_rect()
+    feelslikepos.centerx = background.get_rect().centerx/4
+    feelslikepos.centery = background.get_rect().centery-25
+    background.blit(feelslike, feelslikepos)
+    
 
-
+#Get forecast data from Weather Underground
 def getforecastdata():
     #Call Weather Underground forecast API
     forecasturl = 'http://api.wunderground.com/api/'+api_key+'/forecast/q/zmw:60015.1.99999.json'
@@ -122,26 +131,61 @@ def getforecastdata():
         print '==== Failure to Retrieve ===='
     return fjs
     
+#Display forecast
 def displayforecast(f_json):
-    #Read high temp and display the data
+    #Read today's high temp and display the data
     hightemp_data = f_json["forecast"]["simpleforecast"]["forecastday"][0]["high"]["fahrenheit"]+unichr(176)
     hightemp = base_font.render(hightemp_data, 1, (10, 10, 10))
     hightemppos = hightemp.get_rect()
     hightemppos.centerx = background.get_rect().centerx*1.8
-    hightemppos.centery = background.get_rect().centery/1.5
+    hightemppos.centery = background.get_rect().centery-60
     background.blit(hightemp, hightemppos)
-    #Read low temp and display the data
+    #Read today's low temp and display the data
     lowtemp_data = f_json["forecast"]["simpleforecast"]["forecastday"][0]["low"]["fahrenheit"]+unichr(176)
     lowtemp = base_font.render(lowtemp_data, 1, (10, 10, 10))
     lowtemppos = lowtemp.get_rect()
     lowtemppos.centerx = background.get_rect().centerx*1.8
-    lowtemppos.centery = background.get_rect().centery
-    background.blit(lowtemp, lowtemppos) 
+    lowtemppos.centery = background.get_rect().centery-30
+    background.blit(lowtemp, lowtemppos)
+    #Read 
+    i = 1
+    thirds = background.get_rect().right/3
+    while i < 4:
+        #Display day of week
+        fday_data = f_json["forecast"]["simpleforecast"]["forecastday"][i]["date"]["weekday_short"]
+        fday = small_font.render(fday_data, 1, (10,10,10))
+        fdaypos = fday.get_rect()
+        fdaypos.centerx = thirds*i-80
+        fdaypos.centery = background.get_rect().centery*1.3
+        background.blit(fday, fdaypos)
+        
+        #Display weather icon
+        icon_url = f_json["forecast"]["simpleforecast"]["forecastday"][i]["icon_url"]
+        icon_str = urllib.urlopen(icon_url).read()
+        icon_file = io.BytesIO(icon_str)
+        icon_image = pygame.image.load(icon_file)
+        #icon_image = pygame.transform.scale(icon_image, (100,100))
+        iconpos = icon_image.get_rect()
+        iconpos.centerx = thirds*i-80
+        iconpos.centery = background.get_rect().centery*1.5
+        background.blit(icon_image, iconpos)        
+        
+        #High Low Temp
+        fhightemp_data = f_json["forecast"]["simpleforecast"]["forecastday"][i]["high"]["fahrenheit"]+unichr(176)
+        flowtemp_data = f_json["forecast"]["simpleforecast"]["forecastday"][i]["low"]["fahrenheit"]+unichr(176)
+        high_low = fhightemp_data + "| " + flowtemp_data    
+        fhighlowtemp = small_font.render(high_low, 1, (10, 10, 10))
+        fhighlowtemppos = fhighlowtemp.get_rect()
+        fhighlowtemppos.centerx = thirds*i-80
+        fhighlowtemppos.centery = background.get_rect().centery*1.8
+        background.blit(fhighlowtemp, fhighlowtemppos)
+
+        i = i+1
+
 
 #Retrieve initial data 
 jsoninfo = getcurrentconditionsdata()
 jsonforecast = getforecastdata()
-
 current_time = time.time()  
 
 while not quit:
